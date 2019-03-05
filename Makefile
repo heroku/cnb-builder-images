63041ea34cae54985fadb73266840b51eab2ddc0
Joe Kutner <jpkutner@gmail.com>
2019-03-05 16:24:48 -0600

    Added support cnb-shim for Node.js buildpack

--- Makefile
+++ Makefile
@@ -2,14 +2,17 @@
 
 SHELL=/bin/bash -o pipefail
 
-build:
+install-buildpacks:
+	@bash buildpacks/install.sh
+
+build: install-buildpacks
 	@docker pull heroku/heroku:18-build
 	@docker build -f Dockerfile.build -t heroku/pack:18-build .
 	@docker build -f Dockerfile.run -t heroku/pack:18 .
 	@docker build -f Dockerfile.cache -t heroku/buildpacks:cache .
 	-pack delete-stack heroku-18
 	@pack add-stack heroku-18 --build-image heroku/pack:18-build --run-image heroku/pack:18
-	@pack create-builder heroku/buildpacks:18 -s heroku-18 --builder-config builder.toml --no-pull
+	@pack create-builder heroku/buildpacks:18 --builder-config builder.toml --no-pull
 	@docker tag heroku/buildpacks:18 heroku/buildpacks:latest
 
 publish: build
---
+++ buildpacks/install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+buildpacks_dir="$(cd $(dirname $0); pwd)" # absolute path
+cnb_shim_version="v0.0.1"
+
+fetch_shim() {
+  shim_dir="${1:?}"
+  curl -sfL "https://github.com/heroku/cnb-shim/releases/download/${cnb_shim_version}/cnb-shim-${cnb_shim_version}.tgz" | tar xz -C "${shim_dir}"
+}
+
+install_buildpack() {
+  buildpack="${1:?}"
+  buildpack_name="${2:?}"
+  shim_dir="${3:?}"
+
+  buildpack_dir="${buildpacks_dir}/${buildpack}"
+  rm -rf "${buildpack_dir}"
+  cp -R "${shim_dir}" "${buildpack_dir}"
+
+  buildpack_toml="$(mktemp)"
+  cat > "${buildpack_toml}" << TOML
+[buildpack]
+id = "heroku/${buildpack}"
+version = "0.1"
+name = "${buildpack_name}"
+
+[[stacks]]
+id = "heroku-18"
+TOML
+
+  bash "${buildpack_dir}"/bin/install "${buildpack_toml}" "https://buildpack-registry.s3.amazonaws.com/buildpacks/heroku/${buildpack}.tgz"
+  rm "${buildpack_toml}"
+}
+
+shim_dir="$(mktemp -d)"
+fetch_shim "${shim_dir}"
+install_buildpack "nodejs" "Node.js" "${shim_dir}"
+rm -rf "${shim_dir}"
